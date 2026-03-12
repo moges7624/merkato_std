634e878c8a639c7d2e11a38722391e628a29a832
moges <moges7624@gmail.com>
2026-03-12 14:30:34 +0300

    feat: initializing db

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,25 +1,44 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log/slog"
 	"net/http"
 	"os"
+
+	_ "github.com/lib/pq"
 )
 
 type APIServer struct {
 	port   int
 	routes http.ServeMux
 	logger *slog.Logger
+	dsn    string
 }
 
 func main() {
 	var port int
+	var dsn string
 
 	flag.IntVar(&port, "port", 4000, "API server port")
+	flag.StringVar(
+		&dsn,
+		"db-dsn",
+		"postgres://merkato:123456@localhost/merkatostd?sslmode=disable",
+		"PostgreSQL DSN",
+	)
 
 	logHanlder := slog.NewJSONHandler(os.Stdout, nil)
 	logger := slog.New(logHanlder)
+
+	db, err := openDB(dsn)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+
+	defer db.Close()
 
 	app := &APIServer{
 		port:   port,
@@ -30,9 +49,23 @@ func main() {
 
 	app.logger.Info("Starting server", "port", app.port)
 
-	err := app.serve()
+	err = app.serve()
 	if err != nil {
 		app.logger.Error(err.Error())
 		os.Exit(1)
 	}
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/moges7624/merkato_std
 go 1.25.3
 
 require github.com/brianvoe/gofakeit/v7 v7.14.1
+
+require github.com/lib/pq v1.11.2 // indirect
